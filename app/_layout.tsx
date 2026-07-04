1dc3906ca6231580092970b9bff28e98d94d2fd6
Tran Hoang Nhat <thndev05@gmail.com>
2026-07-04 12:02:18 +0700

    refactor: remove unused components and files; update layout with new structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,66 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { FlatList, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const lists = [
+    { id: 1, house: "Villa", location: 'Beverly Hills' },
+    { id: 2, house: "Apartment", location: 'New York' },
+    { id: 3, house: "Cottage", location: 'Lake District' },
+    { id: 4, house: "Mansion", location: 'Los Angeles' },
+    { id: 5, house: "Bungalow", location: 'Florida' },
+  ];
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+      <View style={{ padding: 16 }}>
+        <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+          thndev05 - learning React Native.
+        </Text>
+
+        <TextInput 
+          placeholder="Enter something ..." 
+          placeholderTextColor="#888" 
+          style={{
+            borderWidth: 1,
+            borderColor: "#ccc",
+            padding: 20,
+            marginTop: 16,
+            borderRadius: 8,
+          }}
+        />
+        
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#007AFF",
+            padding: 16,
+            marginTop: 16,
+            borderRadius: 8,
+            alignItems: "center",
+          }}
+
+          onPress={() => alert("Button pressed!")}
+        >
+          <Text style={{ color: "#fff", fontWeight: "bold" }}>Click!</Text>
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={lists}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              padding: 16,
+              borderBottomWidth: 1,
+              borderBottomColor: "#ccc",
+            }}
+          >
+            <Text style={{ fontSize: 16, fontWeight: "bold" }}>{item.house}</Text>
+            <Text style={{ fontSize: 14, color: "#666" }}>{item.location}</Text>
+          </View>
+        )}
+      />
+
+    </SafeAreaView>
+  )
 }
